99e5ee9fbb6c933cfb68f636b4d6acdfe3698ea5
Rilind Ramushi <rilind.ramushi@gmail.com>
2017-10-18 19:35:15 +0200

    upload i pare i projektit

--- Piano/ViewController.swift
+++ Piano/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer:AVAudioPlayer!
+    var notat = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var butoniIshtypur:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
-
+    @IBAction func playSound(_ sender: UIButton) {
+        
+        
+        butoniIshtypur = sender.tag
+        playSound()
+        
+        
+    }
+    
+    func playSound(){
+        var audioFile = Bundle.main.url(forResource: notat[butoniIshtypur], withExtension: "wav")
+        audioPlayer = try! AVAudioPlayer(contentsOf: audioFile!)
+        audioPlayer.play()
+    }
 }
 
